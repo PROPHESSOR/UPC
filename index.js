6cfd7c8102758628164074aafbc41c428d6753bd
PROPHESSOR <xenteze@gmail.com>
2018-01-13 04:45:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2018 prophessor
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+'use strict';
+
+const fs = require('fs');
+
+const args = process.argv.splice(process.execArgv.length + 2);
+const [TYPE, FROM, TO] = args;
+
+if (!(TYPE && FROM && TO)) return console.log('usage: <type> <from> <to>');
+
+let Translator;
+
+try {
+	Translator = require(`./langs/${TYPE}`);
+} catch (e) {
+	console.error(e);
+	console.error(`\n\n\nUnknown convert type ${TYPE}`);
+}
+
+const translator = new Translator(fs.readFileSync(FROM, 'utf8'));
+
+fs.writeFileSync(TO, translator.translate(), 'utf8');
+
+console.info("OK!");
+
+return 0;
